f54aace7318e97cfcb1556ec211793490a4d5557
Samiul Hasan <samiulxhasan650@gmail.com>
2026-06-27 18:18:43 +0600

    Project Initialize

---
+++ src/mainViews/home/HomeMainView.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const HomeMainView = () => {
+    return (
+        <div>
+            <h1 className='text-2xl'>Real Nest</h1>
+        </div>
+    )
+};
+
+export default HomeMainView;
